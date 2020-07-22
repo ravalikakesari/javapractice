3a8872f8d1392a5d33719570597c780e80adc111
ravalika <ravalika@21.com>
2020-07-22 12:45:06 +0530

    DateTime Api

---
+++ DateTimeDemo.java
@@ -0,0 +1,9 @@
+import java.time.LocalDate;
+public class DateTimeDemo {
+    public static void main(String args[])
+    {
+        LocalDate d = LocalDate.now();
+        System.out.println(d);
+    }
+
+}
