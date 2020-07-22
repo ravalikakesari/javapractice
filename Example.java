a0a8f199d91f90bcb3d4429761caf4d03d6afded
ravalika <ravalika@21.com>
2020-07-22 12:10:38 +0530

    using interfcae and lambda expression

---
+++ Example.java
@@ -0,0 +1,17 @@
+ interface MyFunctionalInterface {
+
+        //A method with no parameter
+        public String sayHello();
+    }
+    public class Example {
+
+        public static void main(String args[]) {
+            // lambda expression
+            MyFunctionalInterface msg = () -> {
+                return "Hello";
+            };
+            System.out.println(msg.sayHello());
+        }
+    }
+
+
